2fcb905091aa4f15cb0d5803507a8c764c79f284
precious douglas <preciousdouglas002@gmail.com>
2023-10-16 13:29:50 +0100

    final project

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "eesgg44hh",
+    name: "14 inches Bone straight hair",
+    price: 90,
+    desc: "It is a good quality straight hair with fine texture,tangle free and doesn't shed.",
+    img: "download.jpg",
+  },
+  {
+    id: "eesh2345h",
+    name: "10 inches Pixie curls",
+    price: 65,
+    desc: "This Curly Hair wig is made with a good quality hair blend, it has a fine texture, it doesn't tangle and it doesn't , shed, it's strands  doesn't get rough and it doesn't change colour, it is versatile, it can be maintained with ordinary water, it lasts longer .",
+    img: "images (1).jpg",
+  },
+  {
+    id: "e5522eshh",
+    name: "12 inches Piano bone straight",
+    price: 42,
+    desc: "This is our finest collection of Premium Lushious human Soft Hair made with silky texture for wear which is at a purse friendly and affordable price just for you. This hair is very versatile for all occasion, easy to style it gives a unique and eye catching look.This hair can be worn straight or curled to your preference.",
+    img: "images.jpg",
+  },
+  {
+    id: "ee3455shh",
+    name: "20 inches Deep wavy Hair",
+    price: 52,
+    desc: "This hair is a must have. It is so comfortable to the skin, durable, and stylish for the classic woman. With a brush of wide tooth comb, your hair maintains its silky textur.",
+    img: "1.jpg",
+  },
+];
